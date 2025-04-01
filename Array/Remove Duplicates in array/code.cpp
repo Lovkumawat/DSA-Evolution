7840b0a0fcf979a4f640b5c7cd7a48ded2d35ef7
lovkumawat <kumawatlov6@gmail.com>
2025-04-01 22:03:35 +0530

    Remove Duplicate in array commite successfully

---
+++ Array/Remove Duplicates in array/code.cpp
@@ -0,0 +1,14 @@
+vector<int> removeDuplicate(vector<int>& arr) {
+    // code here
+    int n=arr.size();
+    vector<int>ans;
+    unordered_map<int,int>mp;
+   
+  for(int i=0;i<n;i++){
+      if(mp[arr[i]]==0){
+          ans.push_back(arr[i]);
+          mp[arr[i]]++;
+      }
+  }
+    return ans;
+}
