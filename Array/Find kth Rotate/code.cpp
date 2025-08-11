4188edd4c53128129df8277aebba6e52d96281d9
lovkumawat <kumawatlov6@gmail.com>
2025-08-11 23:09:11 +0530

    Find kth Rotate commite successfully

---
+++ Array/Find kth Rotate/code.cpp
@@ -0,0 +1,11 @@
+    int findKRotation(vector<int> &arr) {
+        // Code Here
+       
+        for(int i=0;i<arr.size()-1;i++){
+             if(arr[i]>arr[i+1]){
+                return i+1;
+            }
+        }
+        return 0;
+    }
+};
