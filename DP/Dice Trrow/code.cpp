a7d92b8f292a96a55390cddd9d63d2395c9d5d88
lovkumawat <kumawatlov6@gmail.com>
2025-08-17 22:03:09 +0530

    Dice Throw commit successfully

---
+++ DP/Dice Trrow/code.cpp
@@ -0,0 +1,39 @@
+class Solution {
+  public:
+  int solve(int dice,int face,int target,vector<vector<int>>&dp){
+      if(target<0){
+          return 0;
+      }
+      
+      if(dice==0&&target!=0){
+          return 0;
+      }
+      
+      if(target==0&&dice!=0){
+          return 0;
+      }
+      
+      if(target==0&&dice==0){
+          return 1;
+      }
+      
+      if(dp[dice][target]!=-1){
+          return dp[dice][target];
+      }
+      int ans=0;
+      
+      for(int i=1;i<=face;i++){
+          ans=ans+solve(dice-1,face,target-i,dp);
+      }
+      
+      return dp[dice][target]=ans;
+  }
+    int noOfWays(int m, int n, int x) {
+        // code here
+        int size=pow(m,n);
+        vector<vector<int>>dp(n+1,vector<int>(x+1,-1));
+        return solve(n,m,x,dp);
+        
+        
+    }
+};
